139476270cf42c64d9c3006a59eed775e947b004
zemoa <zemoa@netc.fr>
2023-07-03 23:16:02 +0200

    display tasks loading

--- src/app/shared/components/task-item/task-item.component.ts
+++ src/app/shared/components/task-item/task-item.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class TaskItemComponent {
   @Input() loadingMode = true
+  @Input() title!: string
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,10 +8,11 @@ import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { SkeletonModule } from 'primeng/skeleton';
+import { CardModule } from "primeng/card";
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective, TaskItemComponent],
-  imports: [CommonModule, TranslateModule, FormsModule, ButtonModule, SkeletonModule],
+  imports: [CommonModule, TranslateModule, FormsModule, ButtonModule, SkeletonModule, CardModule],
   exports: [TranslateModule, WebviewDirective, FormsModule, ButtonModule, TaskItemComponent]
 })
 export class SharedModule {}
